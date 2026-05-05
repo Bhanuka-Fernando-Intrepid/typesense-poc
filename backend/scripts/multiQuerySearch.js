b6d1b095b475005435e0c98876f5696401a381f5
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-05-05 16:49:05 +0530

    calculate and display network and backend Processing Time for multi search

--- backend/scripts/multiQuerySearch.js
+++ backend/scripts/multiQuerySearch.js
@@ -29,14 +29,25 @@ async function runMultiSearchTests() {
 
     console.log("\n--- Query 1: 'bolivia' (dev collection) ---");
     console.log("Total Found:", response.results[0].found);
+    const p1 = response.results[0].search_time_ms;
+    console.log("Search Time:", p1, "ms");
 
     console.log("\n--- Query 2: 'beach' (test collection) ---");
     console.log("Total Found:", response.results[1].found);
+    const p2 = response.results[1].search_time_ms;
+    console.log("Search Time:", p2, "ms");
 
     console.log("\n--- Query 3: 'north india' (dev collection) ---");
     console.log("Total Found:", response.results[2].found);
+    const p3 = response.results[2].search_time_ms;
+    console.log("Search Time:", p3, "ms");
 
-    console.log("\nTotal Multi-Search Time:", totalTime, "ms");
+    console.log("\nTotal Multi-Search Time (End-to-End):", totalTime, "ms");
+    
+    const backendTime = p1+p2+p3;
+    const overhead = totalTime - backendTime;
+    console.log("Total Backend Processing Time (Inside Typesense Engine Only):", backendTime, "ms");
+    console.log("Network Time:", overhead, "ms");
   } catch (err) {
     console.error("Multi Search error:", err);
   }
